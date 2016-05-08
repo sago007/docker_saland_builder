89057e06b1b8fce7e01e92813852d2ee0a8b8adb
sago007 <poul@poulsander.com>
2016-05-08 14:56:33 +0200

    Only the end result should be in /data. The temp result should be in /staging

--- engine_compile_script.sh
+++ engine_compile_script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -euo pipefail
-cd /data
+cd /staging
 git clone git://github.com/OpenArena/engine.git
 cd engine
 VERSION_ID=$(git log -1 --date=short --format="openarena_engine_%cd_%H")
@@ -8,10 +8,12 @@ echo $VERSION_ID
 STAGING_DIR=/staging/$VERSION_ID
 mkdir $STAGING_DIR
 make
-mv /data/engine/build/release-linux-x86_64/oa_ded.x86_64 $STAGING_DIR/
-mv /data/engine/build/release-linux-x86_64/openarena.x86_64 $STAGING_DIR/
-mv /data/engine/build/release-linux-x86_64/renderer_*_x86_64.so $STAGING_DIR/
-cd /data/engine
-CC=i686-w64-mingw32-gcc ARCH=x86 PLATFORM=mingw32  make
+mv /staging/engine/build/release-linux-x86_64/oa_ded.x86_64 $STAGING_DIR/
+mv /staging/engine/build/release-linux-x86_64/openarena.x86_64 $STAGING_DIR/
+mv /staging/engine/build/release-linux-x86_64/renderer_*_x86_64.so $STAGING_DIR/
+#Windows version does not work at the moment
+#cd /staging/engine
+#CC=i686-w64-mingw32-gcc ARCH=x86 PLATFORM=mingw32  make
+cd /staging
 zip -r $VERSION_ID $VERSION_ID
 mv $VERSION_ID.zip /data/
