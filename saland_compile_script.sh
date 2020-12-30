3e31e23472a8855ced07228162f4ad0189b34546
sago007 <poul@poulsander.com>
2020-12-30 14:04:30 +0100

    Use a fixed windows dir and use the new salandgame org

--- saland_compile_script.sh
+++ saland_compile_script.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -euo pipefail
 cd /staging
-git clone https://github.com/sago007/saland.git
+git clone https://github.com/salandgame/saland.git
 cd saland
 VERSION_ID=$(git log -1 --date=short --format="saland_%cd_%h")
 echo $VERSION_ID
@@ -11,7 +11,7 @@ then
  echo "$ZIPFILE exist -> UP TO DATE"
  exit 1
 fi
-STAGING_DIR=/staging/$VERSION_ID
+STAGING_DIR=/staging/saland_windows
 mkdir $STAGING_DIR
 cp -r /usr/include/utf8 /staging/saland/src/Libs/include/ && \
 cp /usr/include/utf8.h /staging/saland/src/Libs/include/
@@ -20,6 +20,6 @@ make
 mv /staging/saland/saland.exe $STAGING_DIR/
 cp -r /staging/saland/data $STAGING_DIR/
 cd /staging
-zip -r $VERSION_ID $VERSION_ID
+zip -r $VERSION_ID saland_windows
 chmod 644 $VERSION_ID.zip
 mv $VERSION_ID.zip /data/
